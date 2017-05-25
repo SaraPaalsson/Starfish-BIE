0c110b58ac87f85847b8ab31897ae323cf9e2683
SaPa <sara.paalsson@gmail.com>
2017-05-25 10:16:47 +0200

    Removed c files

--- src/matlab/do_spec_quad.m
+++
@@ -1,3 +0,0 @@
-function [] = do_spec_quad()
-
-end
--- src/matlab/mex/my_mexmake.m
+++
@@ -1,5 +0,0 @@
-% mex CXXFLAGS="\$CXXFLAGS -msse4.1 -falign-loops=16 -Wall -fopenmp" ...
-%     DYLDFLAGS="\$DYLDFLAGS -fopenmp" mex_do_force_quad.cpp  
-% mex CXXFLAGS="\$CXXFLAGS -msse4.1 -falign-loops=16 -Wall -fopenmp" -lgomp mex_do_force_quad.cpp 
-
-mex CXXFLAGS="\$CXXFLAGS -O3  -msse4.1 -Wall " mex/mex_saraspecquad.cpp 
